7da112a226a348de398fb8ce5b3bdb47f30678b9
Hiroki Ishiwatari <dev.ish1r0k1@gmail.com>
2018-03-13 23:48:15 +0900

    Change directory structure

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,16 +3,12 @@
 import { src, dest, series, parallel } from "gulp";
 import gutil from "gulp-util";
 import gulpLoadPlugins from "gulp-load-plugins";
-import browserify from "browserify";
-import watchify from "watchify";
-import source from "vinyl-source-stream";
-import buffer from "vinyl-buffer";
 import minimist from "minimist";
 import del from "del";
-import runSequence from "run-sequence";
 import browserSync from "browser-sync";
-import webpack from 'webpack';
-import webpackStream from 'webpack-stream';
+import webpack from "webpack";
+import webpackStream from "webpack-stream";
+import path from 'path';
 
 const $ = gulpLoadPlugins(),
   bs = browserSync.create(),
@@ -30,59 +26,59 @@ const options = minimist(process.argv.slice(2), minimistOptions),
 
 gutil.log("Environment:", gutil.colors.yellow(options.env));
 
-const PATHS = {
-  styles: {
-    scss: "assets/_scss",
-    css: "assets/css"
-  },
-  scripts: {
-    altJS: "assets/_es6",
-    js: "assets/js"
-  },
-  html: "**/*.html"
-};
-
-const AUTOPREFIXER_BROWSER = ["last 2 versions"];
-
 export const styles = () => {
-  return src(`${PATHS.styles.scss}/*.scss`)
+  return src(`src/assets/sass/*.scss`)
     .pipe($.plumber())
     .pipe($.if(!isProduction, $.sourcemaps.init()))
     .pipe($.sass().on("erorr", $.sass.logError))
     .pipe($.postcss())
     .pipe($.if(!isProduction, $.sourcemaps.write("./")))
-    .pipe(dest(PATHS.styles.css))
+    .pipe(dest(`dist/assets/css`))
     .pipe(bs.stream());
 };
 
 export const serve = () => {
   return bs.init({
     server: {
-      baseDir: "./"
+      baseDir: path.resolve(__dirname, 'dist')
     }
   });
 };
 
 export const scripts = () => {
-  return src(`${PATHS.scripts.altJS}/index.js`)
-    .pipe(webpackStream({
-      mode: options.env,
-      output: {
-        filename: '[name].bundle.js'
-      }
-    }, webpack))
-    .pipe(dest(PATHS.scripts.js))
-}
+  return src(`src/assets/js/index.js`)
+    .pipe(
+      webpackStream(
+        {
+          mode: options.env,
+          output: {
+            filename: "[name].bundle.js"
+          }
+        },
+        webpack
+      )
+    )
+    .pipe(dest(`dist/assets/js`));
+};
+
+export const html = () => {
+  return src(`src/**/*.html`, { base: "src" })
+    .pipe(dest("dist"))
+    .pipe($.if(bs.active, bs.reload));
+};
 
 export const watch = () => {
-  $.watch(`${PATHS.styles.scss}/**/*.scss`, styles)
-  $.watch(`${PATHS.scripts.altJS}/**/*.js`, scripts)
-    $.watch(`${PATHS.html}`, reload);
+  $.watch(`src/assets/sass/**/*.scss`, styles);
+  $.watch(`src/assets/js/**/*.js`, scripts);
+  $.watch(`src/**/*.html`, html);
 };
 
-export const clean = () => {
-  return del([`${PATHS.scripts.js}/*.js`, `${PATHS.styles.css}`]);
-};
+export const clean = () => del([`dist`]);
+
+export const copyFiles = () =>
+  src(`src/assets/images/**`, { base: "src" }).pipe(dest("dist"));
+
+export const build = series(clean, html, styles, scripts, copyFiles);
 
 const errorHandler = function(err) {
   gutil.log(gutil.colors.red(`Error: ${err}`));
@@ -91,8 +87,8 @@ const errorHandler = function(err) {
 
 export default (() => {
   if (!isProduction) {
-    return series(clean, parallel(styles, scripts), parallel(serve, watch));
+    return series(build, parallel(serve, watch));
   } else {
-    return series(clean, parallel(styles, scripts));
+    return series(build);
   }
 })();
