aac6081d98aaf59914215f7dcb8831dac9a49ce4
Hiroki Ishiwatari <dev.ish1r0k1@gmail.com>
2019-10-13 16:01:15 +0900

    Migration from Gulp to Webpack

---
+++ webpack/webpack.common.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: [path.resolve(__dirname, '../assets/_es6/index.js')],
+  output: {
+    path: path.join(__dirname, '../public'),
+    filename: 'js/[name].js',
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: path.resolve(__dirname, '../assets/img'), to: 'assets/img' },
+    ]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.(pdf|gif|png|jpe?g|svg)$/,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        // test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[path][name].[ext]',
+          },
+        },
+      },
+    ],
+  },
+}
---
+++ webpack/webpack.config.prod.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack')
+const merge = require('webpack-merge')
+const common = require('./webpack.common')
+const cssnano = require('cssnano')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'js/[name].[chunkhash:8].js',
+    chunkFilename: 'js/[name].[chunkhash:8].chunk.js',
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/,
+      cssProcessor: cssnano,
+      cssProcessorPluginOptions: {
+        preset: [
+          'default',
+          {
+            autoprefixer: {
+              add: true,
+              browsers: ['last 2 versions', 'ie >= 11', 'Android >= 4'],
+            },
+            discardComments: { removeAll: true },
+          },
+        ],
+      },
+      canPrint: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+})
